8446f16fd5607b4c181ce35b1fd12786383e2b9a
jmartinezsusunaga <dub.jona@gmail.com>
2023-07-27 17:13:01 +0200

    Modification scripts sql ajout de données

--- General/Scripts/Script.sql
+++ General/Scripts/Script.sql
@@ -1,4 +1,21 @@
-ALTER TABLE calendrier_tulutz add column id_partenaire_ct;
+/*Créer une colonne*/
+ALTER TABLE calendrier_tulutz ADD COLUMN id_partenaire_ct INTEGER NOT NULL;
 
-add column id_membre_ct,
-FOREIGN KEY (id_membre_ct) REFERENCES info_partenaires(id_mt),
+/*Effacer une colonne*/
+ALTER TABLE calendrier_tulutz  DROP COLUMN partenaire_ct;  
+
+/*Montrer les colonnes d'une table*/
+SHOW COLUMNS FROM calendrier_tulutz; 
+
+/*Ajouter une cléf étrangère*/
+ALTER TABLE calendrier_tulutz ADD FOREIGN KEY (id_partenaire_ct) REFERENCES info_partenaires(id_ip);
+
+ALTER TABLE calendrier_tulutz ADD FOREIGN KEY (id_membre_ct) REFERENCES membres_tulutz(id_mt);
+
+ALTER TABLE calendrier_tulutz ADD FOREIGN KEY (nom_evenement_ct) REFERENCES info_evenements(nom_evenement_ie, date_ie);
+
+/*Une clé primaire répresentée en deux colonnes*/
+ALTER TABLE calendrier_tulutz ADD FOREIGN KEY(nom_evenement_ct, date_ct) REFERENCES info_evenements(nom_evenement_ie, date_ie);
+
+
+
